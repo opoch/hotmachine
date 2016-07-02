669c9e25398fdcd4e2e858eb66587353f5821ee7
opoch <danisiscom@gmail.com>
2016-07-02 10:32:17 -0500

    Este es un nuevo repositorio

--- conexion.php
+++ conexion.php
@@ -11,5 +11,5 @@ $servidor="sql206.260mb.net";
 $BD="n260m_14426545_pcatual";*/
 
 $conexion=mysqli_connect($servidor,$usario,$passwd, $BD)or die("Error al conectarse a la base de datos:".mysqli_error());
-mysql_select_db($BD,$conexion);
+
 ?>
